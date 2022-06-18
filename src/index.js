ccdf13805a424159a3888a2c96a77b6fa29fe5e4
Kat Fay <101537105+katfay@users.noreply.github.com>
2022-06-18 17:49:59 +1000

    Clean up JS
    - Delete console log testing text.
    - Re-test to confirm it still works the same.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 function showSearchedCurrent() {
-  console.log("click is working");
   let apiKey = "a95c2c6739994ba4903e007ee817e7d1";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayName);
-  console.log("search city name API call is working");
   axios.get(apiUrl).then(displayTemperature);
-  console.log("search city current temp API call is working");
 }
 
 function displayName(apiResponse) {
