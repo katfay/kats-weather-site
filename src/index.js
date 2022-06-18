a94940a5572610d2138634157343ebb863355dbc
Kat Fay <101537105+katfay@users.noreply.github.com>
2022-06-18 17:41:09 +1000

    Hide city + temp results until user clicks button
    - Create new <span> for returned current temp results on LHS.
    - Remove placeholder text on LHS.
    - Add JS event listener for when user clicks search button.
    - Add functions to call API results for test city (Paris) and return object properties to the relevant placeholder elements.
    - Test.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-function displayName(apiResponse) {
-  console.log(apiResponse.data.name);
+function showSearchedCurrent() {
+  console.log("click is working");
+  let apiKey = "a95c2c6739994ba4903e007ee817e7d1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayName);
+  console.log("search city name API call is working");
+  axios.get(apiUrl).then(displayTemperature);
+  console.log("search city current temp API call is working");
+}
 
+function displayName(apiResponse) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = apiResponse.data.name;
 }
 
 function displayTemperature(apiResponse) {
-  console.log(apiResponse.data.main.temp);
-
   let temperatureElement = document.querySelector("#searched-temperature");
   temperatureElement.innerHTML = apiResponse.data.main.temp;
   let units = document.querySelector("#units");
   units.innerHTML = "°C";
 }
 
-let apiKey = "a95c2c6739994ba4903e007ee817e7d1";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayName);
-axios.get(apiUrl).then(displayTemperature);
+let searchButton = document.querySelector("#userSearch");
+searchButton.addEventListener("click", showSearchedCurrent);
