ad58b6b6e16d3e705c00714c35d76e07f5a57b4c
Kat Fay <101537105+katfay@users.noreply.github.com>
2022-06-18 17:17:12 +1000

    Test API & JS ids to searched city elements
    - Console.log API data for a test city (Paris) in place of user search city.
    - Console.log API response data for test city object's required properties (city name and current temp).
    - Add id attributes to html elements for city name and current temp results of user search.
    - Test changing inner html of target elements using Paris API results.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
+function displayName(apiResponse) {
+  console.log(apiResponse.data.name);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = apiResponse.data.name;
+}
+
+function displayTemperature(apiResponse) {
+  console.log(apiResponse.data.main.temp);
+
+  let temperatureElement = document.querySelector("#searched-temperature");
+  temperatureElement.innerHTML = apiResponse.data.main.temp;
+  let units = document.querySelector("#units");
+  units.innerHTML = "°C";
+}
+
 let apiKey = "a95c2c6739994ba4903e007ee817e7d1";
-prompt(apiKey + "is your API Key, Kat");
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayName);
+axios.get(apiUrl).then(displayTemperature);
